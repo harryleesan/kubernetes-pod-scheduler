8b42ec03559a25d9a79e7544e42df808bbd14415
Harry Lee <harry.lee.san.temp@gmail.com>
2018-06-20 11:02:36 +0200

    Updated scaling logs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"k8s.io/api/extensions/v1beta1"
@@ -16,16 +17,27 @@ var clientset *kubernetes.Clientset
 
 func scale(d v1beta1.Deployment) error {
 	if d.ObjectMeta.Annotations["scaleDown"] != "" && d.ObjectMeta.Annotations["scaleUp"] != "" {
+
 		fmt.Printf("Deployment: %#v\nNamespace: %#v\n", d.ObjectMeta.Name, d.ObjectMeta.Namespace)
 		fmt.Printf("Scale Down: %#v\n", d.ObjectMeta.Annotations["scaleDown"])
 		fmt.Printf("Scale Up: %#v\n", d.ObjectMeta.Annotations["scaleUp"])
+
 		namespace := d.ObjectMeta.Namespace
 		name := d.ObjectMeta.Name
+
+		if os.Getenv("SCALE") == "scaleUp" {
+			fmt.Println("Scaling up...")
+		} else if os.Getenv("SCALE") == "scaleDown" {
+			fmt.Println("Scaling down...")
+		} else {
+			return errors.New("Incorrect env var for SCALE!")
+		}
+
 		i, intConvError := strconv.ParseInt(d.ObjectMeta.Annotations[os.Getenv("SCALE")], 10, 32)
 		if intConvError != nil {
 			return intConvError
 		}
-		scaleRes, err := clientset.ExtensionsV1beta1().Deployments(namespace).UpdateScale(name, &v1beta1.Scale{
+		_, err := clientset.ExtensionsV1beta1().Deployments(namespace).UpdateScale(name, &v1beta1.Scale{
 			TypeMeta:   d.TypeMeta,
 			ObjectMeta: d.ObjectMeta,
 			Spec:       v1beta1.ScaleSpec{int32(i)},
@@ -33,7 +45,7 @@ func scale(d v1beta1.Deployment) error {
 		if err != nil {
 			return err
 		}
-		fmt.Printf("Scaling pods to: %v\n", scaleRes.Status.Replicas)
+		fmt.Printf("Scaled pods to: %v\n", int32(i))
 		fmt.Println("")
 	}
 	return nil
